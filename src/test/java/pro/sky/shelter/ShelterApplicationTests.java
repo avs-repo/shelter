23a70fcb1767caef8999bd23b0a9e34a3a60bfec
Andrew <avs@insperion.ru>
2022-12-15 12:49:42 +0300

    v0.0

---
+++ src/test/java/pro/sky/shelter/ShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
